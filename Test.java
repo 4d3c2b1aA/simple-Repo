1b4068bf310c18f20ac5772cc9401a1dc1940e58
4d3c2b1aA <kokrenamdev72@gmail.com>
2022-11-02 09:55:03 +0530

    all code added

--- Test.java
+++ Test.java
@@ -1,9 +1,17 @@
           
         public class Test{
+<<<<<<< Updated upstream
       public void add(){
      System.out.println("method added");
 
      }
+=======
+      public static void main (String args[]){
+     int a=10;
+     int b=20;
+     int c=a+b;
+       System.out.println(c);
+>>>>>>> Stashed changes
 
 
      }
