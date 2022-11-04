f63cf3be332168ba6767a4508679eee19c2d804d
4d3c2b1aA <99802724+4d3c2b1aA@users.noreply.github.com>
2022-11-04 10:45:05 +0530

    Update Test.java
    add one class

--- Test.java
+++ Test.java
@@ -1,17 +1,3 @@
-          
-        public class Test{
-<<<<<<< Updated upstream
-      public void add(){
-     System.out.println("method added");
-
-     }
-=======
-      public static void main (String args[]){
-     int a=10;
-     int b=20;
-     int c=a+b;
-       System.out.println(c);
->>>>>>> Stashed changes
-
-
-     }
+  public class Test{
+                    
+  }
